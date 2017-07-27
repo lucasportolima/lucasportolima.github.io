0f8cd33bb13364b5b40bf92e0f11caeae1fa46da
root <root@lucasporto-VirtualBox.(none)>
2017-07-27 20:33:01 -0300

    novas atualizações

--- js/scripts.js
+++ js/scripts.js
@@ -97,21 +97,12 @@ function move() {
 //     // body...
 // }
 
+function main() {
+    
+
+}
 
 
- $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true,
-    autoplay: true,
-});
+
+
+ 
